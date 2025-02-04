bbe8bf31e6fdc6b9285c159a90293bd878f7a210
Ana Scolari <127357173+apsscolari@users.noreply.github.com>
2025-02-04 10:34:56 -0800

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ password = 'Password1234'
 
 google_api_token = "AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q"
 
+# fake AWS key for testing, adding the pair on two separated files
+aws_access_key_id = AKIAT4GVSAXXJMW2S4FF
+
 # main
 if __name__ == '__main__':
 
