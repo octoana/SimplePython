8944944877b00351daec54fa9f4a6650111e12fc
Ana Scolari <127357173+apsscolari@users.noreply.github.com>
2025-03-10 17:34:52 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ from dotenv import load_dotenv
 user = 'test'
 password = 'Password1234'
 
+# critical vuln example
+user_input = input("Enter filename to read: ")
+with open(user_input, 'r') as file:  # Vulnerable to directory traversal
+    content = file.read()
+
+
 google_api_token = "AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q"
 gh_PAT = "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"
 
